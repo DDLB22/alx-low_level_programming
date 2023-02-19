d31b7d53c44b06b57339b0e6e8ee0c56877964b8
David Blavo <dlblavo@st.ug.edu.gh>
2023-02-19 10:49:10 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both lower upper cases
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+}
